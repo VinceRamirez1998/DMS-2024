8d59b89fbd3ec5462065daa73922fdcfe096f9ef
Vince Ramirez <it.benedictramirez247@gmail.com>
2024-07-28 14:43:53 +0800

    backend

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -22,7 +22,7 @@
       <div class="hidden w-full md:block md:w-auto" id="navbar-default">
         <ul class="flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
           <li>
-            <a href="#" class="block py-2 px-1 text-white dark:hover:bg-red-500 md:dark:hover:bg-transparent rounded md:p-0" aria-current="page">Home</a>
+            <a href="#" class="block py-2 px-1 text-white dark:hover:bg-red-500 md:dark:hover:bg-transparent rounded md:p-0" aria-current="page">12345</a>
           </li>
           <li>
             <a href="#" class="block py-2 px-1 text-white dark:hover:bg-red-500 md:dark:hover:bg-transparent rounded md:p-0">About Us</a>
