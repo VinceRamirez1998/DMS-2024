7bb64f58f031bc823015b251fda83620f17766c9
Kyle Dylan Roque <kyledylanroque17@gmail.com>
2024-08-24 20:34:12 +0800

    Added Proposals Model

---
+++ app/Models/Proposals.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Proposals extends Model
+{
+    use HasFactory;
+    protected $table = 'proposals';
+    protected $fillable = [
+        'lastname',
+        'firstname',
+        'email',
+        'position',
+        'project_title',
+        'project_description',
+        'file',
+    ];
+}
